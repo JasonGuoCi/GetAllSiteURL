c3758cfe684fe7e62877b39ee86e55f28818b0fe
JasonGuoCi <geqianci@163.com>
2018-03-20 15:35:12 +0800

    add project

---
+++ GetAllSiteURL/GetAllSiteURL/ExportUrl.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GetAllSiteURL
+{
+    partial class ExportUrl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWebUrl = new System.Windows.Forms.Label();
+            this.UrlStr = new System.Windows.Forms.TextBox();
+            this.linkLog = new System.Windows.Forms.LinkLabel();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWebUrl
+            // 
+            this.lblWebUrl.AutoSize = true;
+            this.lblWebUrl.Location = new System.Drawing.Point(26, 13);
+            this.lblWebUrl.Name = "lblWebUrl";
+            this.lblWebUrl.Size = new System.Drawing.Size(68, 13);
+            this.lblWebUrl.TabIndex = 0;
+            this.lblWebUrl.Text = "WebApp Url:";
+            // 
+            // UrlStr
+            // 
+            this.UrlStr.Location = new System.Drawing.Point(113, 13);
+            this.UrlStr.Name = "UrlStr";
+            this.UrlStr.Size = new System.Drawing.Size(341, 20);
+            this.UrlStr.TabIndex = 1;
+            // 
+            // linkLog
+            // 
+            this.linkLog.AutoSize = true;
+            this.linkLog.Location = new System.Drawing.Point(133, 40);
+            this.linkLog.Name = "linkLog";
+            this.linkLog.Size = new System.Drawing.Size(25, 13);
+            this.linkLog.TabIndex = 2;
+            this.linkLog.TabStop = true;
+            this.linkLog.Text = "Log";
+            this.linkLog.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLog_LinkClicked);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(379, 35);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 3;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // ExportUrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 87);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.linkLog);
+            this.Controls.Add(this.UrlStr);
+            this.Controls.Add(this.lblWebUrl);
+            this.Name = "ExportUrl";
+            this.Text = "ExportUrl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWebUrl;
+        private System.Windows.Forms.TextBox UrlStr;
+        private System.Windows.Forms.LinkLabel linkLog;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ GetAllSiteURL/GetAllSiteURL/ExportUrl.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetAllSiteURL
+{
+    public partial class ExportUrl : Form
+    {
+        public ExportUrl()
+        {
+            InitializeComponent();
+            linkLog.Text = string.Empty;
+        }
+        private string logPath;
+        private string errorLogPath;
+        private string webAppUrl;
+        private string resultPath = "SiteUrls.csv";
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            logPath = "CR1052or6Log_" + DateTime.Now.ToString("ddMMyyyyhhmmss") + ".txt";
+            errorLogPath = "CRErrorLog_" + DateTime.Now.ToString("ddMMyyyyhhmmss") + ".txt";
+            if (File.Exists(resultPath))
+            {
+                File.Delete(resultPath);
+            }
+            //ConnStr.Text = @"Data Source=US70TWDBS019\SP13DMIG01;Initial Catalog=SFDC_Integrations;Integrated Security=SSPI;";
+            if (string.IsNullOrEmpty(UrlStr.Text))
+            {
+                MessageBox.Show("Please input Web Application Url.");
+                return;
+            }
+            else
+            {
+                webAppUrl = UrlStr.Text;
+            }
+
+            SetLabelText("Running...Please wait...");
+            btnExport.Enabled = false;
+            linkLog.Enabled = false;
+            Thread thread = new Thread(new ThreadStart(GetUrl));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void GetUrl()
+        {
+            try
+            {
+                SPSecurity.RunWithElevatedPrivileges(delegate()
+                {
+                    SPWebApplication webApp = SPWebApplication.Lookup(new Uri(webAppUrl));
+                    DateTime date1 = System.DateTime.Now;
+                    WriteLog(date1.ToString());
+                    string head = @"url";
+                    WriteResult(head);
+
+                    foreach (SPSite site in webApp.Sites)
+                    {
+
+                        try
+                        {
+                            //string resultText = "\"" + site.Url + "\"";
+                            //WriteResult(resultText);
+                            try
+                            {
+                                DoSubsite(site.RootWeb);
+                            }
+                            catch (Exception ex)
+                            {
+
+                                WriteErrorLog("Into DoSubsite method error for site " + site.Url + ex.ToString());
+                            }
+                            
+                        }
+                        catch (Exception ex)
+                        {
+
+                            WriteErrorLog("write site error for site " + site.Url + ex.ToString());
+                        }
+                        site.Dispose();
+                        site.Close();
+                    }
+                    DateTime date2 = System.DateTime.Now;
+                    WriteLog("End time:" + (date2 - date1).ToString());
+                }
+                );
+
+
+                this.BeginInvoke(new MethodInvoker(delegate
+                {
+                    btnExport.Enabled = true;
+                    linkLog.Enabled = true;
+                }));
+
+                SetLabelText("Click here to view log file.");
+
+            }
+            catch (Exception ex)
+            {
+                WriteErrorLog("There are something wrong!!" + ex.ToString());
+            }
+        }
+
+        private void DoSubsite(SPWeb web)
+        {
+            try
+            {
+                string webServerRelativeUrl = web.ServerRelativeUrl.TrimEnd(new char[] { '/' });
+                if (webServerRelativeUrl != "" && !webServerRelativeUrl.Contains("QuoteMigration") && !webServerRelativeUrl.Contains("OfferMigration") && !webServerRelativeUrl.Contains("ContractMigration"))
+                {
+                    string resultText = "\"" + web.Url + "\"";
+                    WriteResult(resultText);
+                }
+               
+
+                foreach (SPWeb subWeb in web.Webs)
+                {
+                    DoSubsite(subWeb);
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteErrorLog("write Url error for web:" + web.Url + ". Exception:" + ex.ToString());
+            }
+            finally
+            {
+                web.Dispose();
+                web.Close();
+                web = null;
+            }
+        }
+        public void WriteResult(string text)
+        {
+            FileStream fs = new FileStream(resultPath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+            sw.WriteLine(text);
+            sw.Close();
+            fs.Close();
+        }
+        delegate void labDelegate(string str);
+        private void SetLabelText(string str)
+        {
+            if (linkLog.InvokeRequired)
+            {
+                Invoke(new labDelegate(SetLabelText), new string[] { str });
+            }
+            else
+            {
+                linkLog.Text = str;
+            }
+        }
+        public void WriteLog(string text)
+        {
+            FileStream fs = new FileStream(logPath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+            sw.WriteLine(text);
+            sw.Close();
+            fs.Close();
+        }
+        public void WriteErrorLog(string text)
+        {
+            FileStream fs = new FileStream(errorLogPath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+            sw.WriteLine(text);
+            sw.Close();
+            fs.Close();
+        }
+
+        private void linkLog_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(logPath);
+        }
+    }
+}
